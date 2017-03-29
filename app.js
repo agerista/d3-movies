c6061574fcc6f3cb99407ad244252b339a3ca749
agerista <amandacrawfordcodes@gmail.com>
2017-03-29 01:48:12 +0000

    shapes

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var quotes = [
-    "Go ahead, make my day.",
-    "I'll be back.",
-    "May the Force be with you.",
-    "There's no place like home.",
-    "You're gonna need a bigger boat."
-];
+var quotes = [{
+    quote: "Go ahead, make my day.",
+    color: "red"
+}, {
+    quote: "I'll be back.",
+    color: "orange"
+}, {
+    quote: "May the Force be with you.",
+    color: "yellow"
+}, {
+    quote: "There's no place like home.",
+    color: "green"
+}, {
+    quote: "You're gonna need a bigger boat.",
+    color: "blue"
+}];
 
 d3.select("body")
   .selectAll("p")
@@ -12,5 +21,14 @@ d3.select("body")
   .enter()
   .append("p")
   .text(function(d) {
-    return d;
+    return d.quote;
+  })
+
+  .style('color', function(d) {
+    return d.color;
   });
+
+  .style("font-size", function() {
+    return Math.round(((Math.random() * 20) + 10) + "px")
+  });
+
